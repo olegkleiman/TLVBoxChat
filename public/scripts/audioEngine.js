52216c9e664a6f3814137647ad5df0be374496fe
Oleg Kleiman <oleg_kleyman@hotmail.com>
2025-01-17 18:54:44 +0200

    audio engine callbacks

--- public/scripts/audioEngine.js
+++ public/scripts/audioEngine.js
@@ -1,28 +1,67 @@
+//
+//  audioEngine.js
+//
+//  Created by Oleg on 17/01/2025.
+//
+
 class AudioEngine {
-    constructor(language) {
+    constructor(language, 
+                recognitionStartedCallback, 
+                recognitionEndedCallback,
+                recornition_resultCallback) {
         this.lang = language;
+        this.recognizing = false;
 
         // Initialize the speech recognition
-        this.recognition = new (window.webkitSpeechRecognition || 
+        if( 'webkitSpeechRecognition' in window )
+        {
+            this.recognition = new (window.webkitSpeechRecognition || 
                     window.webkitSpeechRecognition ||
                     window.mozSpeechRecognition ||
                     window.msSpeechRecognition)();
 
-            this.recognition.continuous = false; // when the user stops talking, speech recognition will end.
+            this.recognition.continuous = true; // when the user stops talking, speech recognition will end.
             this.recognition.interimResults = false; // if false only results returned by the recognizer are final and will not change. 
+            this.recognition.lang = language;
+
+            this.recognition.onstart = () => {
+                console.log('Speech recognition started');
+                this.recognizing = true;
+
+                recognitionStartedCallback?.call(this);
+            }
+            
+            this.recognition.onend = () => {
+                console.log('Speech recognition ended');
+                this.recognizing = false;
+                // this.recognition.stop();
+
+                recognitionEndedCallback?.call(this);
+            }
+
+            this.recognition.onerror = (event) => {
+                console.error(`Speeech recognition error: ${event.error}`);
+                this.recognizing = false;
+                recognitionEndedCallback?.call(this);
+            };
 
             this.recognition.onresult = (event) => {
+                console.log('Speech recognition result');
                 let final_transcript = '';
                 let interim_transcript = '';
 
                 for (var i = event.resultIndex; i < event.results.length; ++i) {
                     if (event.results[i].isFinal) {
                         final_transcript += event.results[i][0].transcript;
+                        console.log(final_transcript);
+                        this.recognition.stop();
+                        recornition_resultCallback?.call(this, final_transcript);
                         speak(final_transcript)
                     } else {
                         interim_transcript += event.results[i][0].transcript;
                     }
                 }
+            }
         }
 
         // Initialize the speech synthesis
@@ -34,6 +73,10 @@ class AudioEngine {
     }
 
     startRecognition = () => {
+        if (this.recognizing) {
+            this.recognition.stop();
+            return;
+        }
         this.recognition.start();
     }
 
