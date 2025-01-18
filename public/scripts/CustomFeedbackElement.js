472799e6036fec385e371d4546a49be551eb845d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2025-01-18 03:22:24 +0200

    feedback button with styles

--- public/scripts/CustomFeedbackElement.js
+++ public/scripts/CustomFeedbackElement.js
@@ -14,6 +14,7 @@ class CustomFeedbackElement extends HTMLElement {
       // Build the component as required.
       const button = document.createElement('button');
       button.innerText = 'Submit';
+      button.style = "border-color: rgb(42, 39, 218); background-color: white; border-radius: 8px; font: -webkit-control;"
       button.addEventListener('click', () => {
         this._onSubmitClick();
       });
